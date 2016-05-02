c39c710084bf77442459541dac079324675b5f85
wills <wills@localhost>
2016-05-02 16:02:07 -0400

    Some Code put in

--- GameCollectionWKS/src/edu/saintjoe/cs/wills/GameCollection/Game.java
+++ GameCollectionWKS/src/edu/saintjoe/cs/wills/GameCollection/Game.java
@@ -1,6 +1,26 @@
 package edu.saintjoe.cs.wills.GameCollection;
 
 public class Game {
-	
+	private String name;
+	private String genre;
 
+
+public Game(String newName, String newGenre) {
+	name = newName;
+	genre = newGenre;
+	
 }
+	public void setName(String newName) {
+	name = newName;
+}
+	public String getName() {
+	return name;
+}
+	public void setGenre(String newGenre) {
+		genre = newGenre;
+	}
+	public String getGenre() {
+		return genre;
+	}
+}
+
